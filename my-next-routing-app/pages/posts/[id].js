2397b2504645161530e2d522ad7a86c4a5730e33
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-03-24 00:11:17 -0400

    modified typo in [id].js

--- my-next-routing-app/pages/posts/[id].js
+++ my-next-routing-app/pages/posts/[id].js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 const posts = [
     { id: '1', title: 'Music Post', content: 'Who can compete with Drake these days?' },
     { id: '2', title: 'Tech Post', content: 'Elon will change what the future holds when it comes to AI development.' },
-    { id: '3', title: 'Food Post', content: 'Italien food is one the best things ever created.' }
+    { id: '3', title: 'Food Post', content: 'Italien food is one the best foods ever created.' }
 ];
 
 export default function Post() {
