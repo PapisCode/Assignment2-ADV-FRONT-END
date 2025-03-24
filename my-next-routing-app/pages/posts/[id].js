66b2a0e849b6b914d91956f207270615c93b01ca
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-03-24 00:08:10 -0400

    Fix and updated dynamic post page loggic

--- my-next-routing-app/pages/posts/[id].js
+++ my-next-routing-app/pages/posts/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-const post = [
+const posts = [
     { id: '1', title: 'Music Post', content: 'Who can compete with Drake these days?' },
     { id: '2', title: 'Tech Post', content: 'Elon will change what the future holds when it comes to AI development.' },
     { id: '3', title: 'Food Post', content: 'Italien food is one the best things ever created.' }
@@ -9,14 +9,15 @@ const post = [
 export default function Post() {
     const router = useRouter();
     const { id } = router.query;
-    const post = post.find(p => p.id === id);
+    
+    const currentPost = posts.find(p => p.id === id);
 
-    if (!post) return <p>Post not found</p>;
+    if (!currentPost) return <p>Post not found</p>;
 
     return (
         <div>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
+            <h1>{currentPost.title}</h1>
+            <p>{currentPost.content}</p>
             <a href="/">Back to Home</a>
         </div>
     );
